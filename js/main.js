723af4f89b314de9dd9c6754590a5aee3b34bc5d
Holly Springsteen <hhspringsteen@gmail.com>
2015-04-23 18:19:03 -0400

    a little more

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 	var htmlWidth = $('html').width();
-	var iframeMargin = htmlWidth / 10 + 65;
+	var iframeMargin = htmlWidth / 10 + 80;
 	
 	$('iframe.desktop').width(htmlWidth);
 	$('.img-frame').width(htmlWidth);
